e7d65f393f9b44785b8265aba17b1712a4375364
Julian Garcia <julian.garcia@pobox.com>
2013-04-04 14:18:48 +0200

    Tests passing.

---
+++ agentbased/src/main/java/com/evolutionandgames/agentbased/impl/CompactPopulationImpl.java
@@ -0,0 +1,71 @@
+package com.evolutionandgames.agentbased.impl;
+
+import java.util.HashMap;
+import java.util.Set;
+
+import com.evolutionandgames.agentbased.Agent;
+import com.evolutionandgames.agentbased.population.CompactPopulation;
+
+public class CompactPopulationImpl implements CompactPopulation {
+	
+	private HashMap<Agent, Integer> populationComposition;
+	private HashMap<Agent, Double> payoffs;
+	
+	//Holder for other info in the run that may be needed
+	private Object extraInfo;
+	
+	
+
+	public int getSize() {
+		Integer sum= 0; 
+	     for (Integer i:populationComposition.values())
+	         sum = sum + i;
+	     return sum;
+	}
+
+	public Set<Agent> getSetOfAgents() {
+		return populationComposition.keySet();
+	}
+
+	public Object getExtraInfo() {
+		return extraInfo;
+	}
+
+	public double getPayoffOfAgent(Agent agent) {
+		return payoffs.get(agent);
+	}
+
+	public void setPayoffOfAgent(Agent agent, double payoff) {
+		this.payoffs.put(agent, payoff);
+	}
+
+	public int getNumberOfCopies(Agent agent) {
+		return this.populationComposition.get(agent);
+	}
+
+	public void setNumberOfCopies(Agent agent, int copies) {
+		this.populationComposition.put(agent, copies);
+
+	}
+
+	public void incrementNumberofCopies(Agent agent) {
+		this.populationComposition.put(agent, this.populationComposition.get(agent)+1);
+
+	}
+
+	public void decrementNumberofCopies(Agent agent) {
+		this.populationComposition.put(agent, this.populationComposition.get(agent)-1);
+	}
+
+	public CompactPopulationImpl(HashMap<Agent, Integer> populationComposition) {
+		super();
+		this.populationComposition = populationComposition;
+		this.payoffs = new HashMap<Agent, Double>();
+		
+	}
+
+	public HashMap<Agent, Integer> getDictionaryOfCopies() {
+		return this.populationComposition;
+	}
+
+}
